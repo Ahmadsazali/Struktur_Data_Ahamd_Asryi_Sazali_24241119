2984dc669816e59b41549927d1029315f531eea8
Ahmadsazali <ahmadarsyi088@gmail.com>
2025-06-26 15:11:38 +0800

    Add files via upload

---
+++ tugas8.py
@@ -0,0 +1,17 @@
+# Data harga
+harga_normal = 15.5935
+harga_diskon = 16.45987
+harga_retail = 14.96884
+
+# Menampilkan dengan 1 digit di belakang koma
+print("1 digit di belakang koma:")
+print("harga_normal =", format(harga_normal, ".1f"))
+print("harga_diskon =", format(harga_diskon, ".1f"))
+print("harga_retail =", format(harga_retail, ".1f"))
+
+# Menampilkan dengan 2 digit di belakang koma
+print("\n2 digit di belakang koma:")
+print("harga_normal =", format(harga_normal, ".2f"))
+print("harga_diskon =", format(harga_diskon, ".2f"))
+print("harga_retail =", format(harga_retail, ".2f"))
+    
